718e3eedf03840025e803a648146b8c2ee74059a
paulguomyth <paulguomyth@gmail.com>
2024-04-13 16:59:16 +0800

    add iris run script

---
+++ iris_run.sh
@@ -0,0 +1,3 @@
+bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_cpu_video_input \
+  --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_cpu_video_input.pbtxt \
+  --input_side_packets=input_video_path=7.mov,output_video_path=7x.mp4
