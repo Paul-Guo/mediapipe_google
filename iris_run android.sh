29c03304dd597caec9a9d34519f3c7c414e42163
paulguomyth <paulguomyth@gmail.com>
2024-04-16 16:57:33 +0800

    add iris run script

--- iris_run android.sh
+++ iris_run android.sh
@@ -1,5 +1,2 @@
-
-bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/iris_tracking:iris_tracking_cpu_video_input
-bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_cpu_video_input \
-  --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_cpu_video_input.pbtxt \
-  --input_side_packets=input_video_path=7.mov,output_video_path=7out.mp4
+bazel build -c opt --config=android_arm64 mediapipe/examples/android/src/java/com/google/mediapipe/apps/iristrackinggpu:iristrackinggpu
+adb install -r bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/iristrackinggpu/iristrackinggpu.apk
