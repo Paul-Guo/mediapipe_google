f8684259fc6cb70edd5161c0bf4769842dd85d24
paulguomyth <paulguomyth@gmail.com>
2024-04-22 13:49:12 +0800

    add flip up download mov

--- iris_run_linux_gpu.sh
+++ iris_run_linux_gpu.sh
@@ -2,4 +2,4 @@
 bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS --copt -DEGL_NO_X11 mediapipe/examples/desktop/iris_tracking:iris_tracking_gpu
 bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_gpu \
   --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_gpu.pbtxt \
-  --input_side_packets=input_video_path=7.mov,output_video_path=out_linux_gpu.mp4
+  --input_side_packets=input_video_path=9.mov,output_video_path=out_linux_gpu.mp4
