f48352e7bd291b0b9a48ff289b34391a09fef444
paulguomyth <paulguomyth@gmail.com>
2024-04-22 14:06:12 +0800

    add gpu render

--- iris_run_linux_gpu.sh
+++ iris_run_linux_gpu.sh
@@ -1,5 +1,5 @@
 
-bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS --copt -DEGL_NO_X11 mediapipe/examples/desktop/iris_tracking:iris_tracking_gpu
-bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_gpu \
-  --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_gpu.pbtxt \
+bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/iris_tracking:iris_tracking_gpu_video_input
+bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_gpu_video_input \
+  --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_gpu_video_input.pbtxt \
   --input_side_packets=input_video_path=9.mov,output_video_path=out_linux_gpu.mp4
