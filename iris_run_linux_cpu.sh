df429e904732fcdce27c4556015b5c4755121c7c
paulguomyth <paulguomyth@gmail.com>
2024-04-22 16:53:04 +0800

    remove mov file

--- iris_run_linux_cpu.sh
+++ iris_run_linux_cpu.sh
@@ -2,5 +2,5 @@
 bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/iris_tracking:iris_tracking_cpu
 bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_cpu \
   --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_cpu.pbtxt \
-  --input_video_path=7.mov \
+  --input_video_path=7.mp4 \
   --output_video_path=out_linux_cpu.mp4
--- iris_run_linux_gpu.sh
+++ iris_run_linux_gpu.sh
@@ -2,5 +2,5 @@
 bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS --copt -DEGL_NO_X11 mediapipe/examples/desktop/iris_tracking:iris_tracking_gpu
 bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_gpu \
   --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_gpu.pbtxt \
-  --input_video_path=7.mov \
+  --input_video_path=7.mp4 \
   --output_video_path=out_linux_gpu.mp4
