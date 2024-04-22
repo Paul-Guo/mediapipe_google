4bbddaaacb978237dae738c4adf4506cf34618d9
paulguomyth <paulguomyth@gmail.com>
2024-04-22 10:08:11 +0800

    add linux cpu shell

---
+++ iris_run_linux_cpu.sh
@@ -0,0 +1,5 @@
+
+bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/iris_tracking:iris_tracking_cpu_video_input
+bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_cpu_video_input \
+  --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_cpu_video_input.pbtxt \
+  --input_side_packets=input_video_path=7.mov,output_video_path=out_linux_cpu.mp4
