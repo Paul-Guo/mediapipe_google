461dc0cbe89ea560b162223b0f1a2f189ff80618
paulguomyth <paulguomyth@gmail.com>
2024-04-22 14:07:25 +0800

    add ubuntu centos video srouce

--- iris_run_linux_gpu.sh
+++ iris_run_linux_gpu.sh
@@ -2,4 +2,4 @@
 bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/iris_tracking:iris_tracking_gpu_video_input
 bazel-bin/mediapipe/examples/desktop/iris_tracking/iris_tracking_gpu_video_input \
   --calculator_graph_config_file=mediapipe/graphs/iris_tracking/iris_tracking_gpu_video_input.pbtxt \
-  --input_side_packets=input_video_path=9.mov,output_video_path=out_linux_gpu.mp4
+  --input_side_packets=input_video_path=9.mov,output_video_path=out_linux_gpu_centos.mp4
