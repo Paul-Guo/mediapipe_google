95b3e93ea43942a5d403e38adcfd0f3ed573faf9
paulguomyth <paulguomyth@gmail.com>
2024-04-24 13:51:53 +0800

    add knift macos run

---
+++ knift_run_mac.sh
@@ -0,0 +1,5 @@
+bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 \
+mediapipe/examples/desktop/template_matching:template_matching_tflite
+bazel-bin/mediapipe/examples/desktop/template_matching/template_matching_tflite \
+--calculator_graph_config_file=mediapipe/graphs/template_matching/template_matching_desktop.pbtxt \
+--input_side_packets="input_video_path=7.mov,output_video_path=out_knift_macos.mp4"
